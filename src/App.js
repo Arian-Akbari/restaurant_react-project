6052f460645bb35441200cb745dfa50eedd84de3
“Arian” <arian09337332108@gmail.com>
2023-10-17 23:07:48 +0330

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+const App = () => {
+    return(
+        <div>
+            <h1>
+                my restaurant
+            </h1>
+        </div>
+    )
+}
+export default App
